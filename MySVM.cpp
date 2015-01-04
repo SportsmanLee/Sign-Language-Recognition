4857987601394b270d63cfa741f4c173df2bd44c
mana1001 <timuncleuncle@hotmail.com>
2015-01-04 16:28:26 +0800

    MySVM.cpp update pca&lda by Tim

--- MySVM.cpp
+++ MySVM.cpp
@@ -51,30 +51,103 @@ void MySVM::clearVectors()
 
 void MySVM::trainSVM()
 {
+	vector<int> classes;
 	// Set up training data
 	Mat labels(gtVectors.size() + otherVectors.size(), 1, CV_32FC1, Scalar(0));
 	for (int i = 0; i < labels.rows; ++i) {
-		labels.at<float>(i, 0) = (i < (int)gtVectors.size()) ? 1.0 : -1.0;
+		labels.at<float>(i, 0) = (i < (int)gtVectors.size()) ? 0 : 1;
+		if(labels.at<float>(i, 0)==0)
+			classes.push_back(0);
+		else
+			classes.push_back(1);
+		//labels.at<float>(i, 0) = (float)i;
+
 	}
 
-	Mat trainingData(gtVectors.size() + otherVectors.size(), gtVectors[0].size(), CV_32FC1);
+	Mat trainingData(gtVectors.size() + otherVectors.size(),  gtVectors[0].size(), CV_32FC1);
+	//Mat trainingData(gtVectors.size() + otherVectors.size(),  7, CV_32FC1);
 	for (int i = 0; i < trainingData.rows; ++i) {
 		for (int j = 0; j < trainingData.cols; ++j) {
 			trainingData.at<float>(i, j) = (i < (int)gtVectors.size()) ? gtVectors[i][j] : otherVectors[i - gtVectors.size()][j];
 		}
 	}
+	
+	int class_num = 2;
 
+	int N = trainingData.rows;
+    int D = trainingData.cols;
+	PCA pca(trainingData , Mat(), CV_PCA_DATA_AS_ROW, N-class_num);
+	//get lda transform matrix 
+	LDA lda=LDA(pca.project(trainingData),classes , 1); 
+	transform = lda.project(pca.project(trainingData));
+
+	transform.convertTo(transform , CV_32FC1);
+	//write transform to txt
+	char filename[]="transform.txt";
+    fstream fp;
+    fp.open(filename, ios::out);//開啟檔案
+    if(!fp){//如果開啟檔案失敗，fp為0；成功，fp為非0
+        cout<<"Fail to open file: "<<filename<<endl;
+    }
+    cout<<"File Descriptor: "<<fp<<endl;
+	for(int i = 0 ; i < transform.rows ; i++)
+	{
+		for(int j = 0 ; j < transform.cols ; j++)
+		{
+			fp<<transform.ptr<float>(i)[j] << '\t';//寫入字串
+		}
+		fp<<endl;
+	}
+  
+ 
+    fp.close();//關閉檔案
+
+
+	//write transform to txt
+	char filename2[]="trainingdata.txt";
+    fstream fp2;
+    fp2.open(filename2, ios::out);//開啟檔案
+    if(!fp2){//如果開啟檔案失敗，fp為0；成功，fp為非0
+        cout<<"Fail to open file: "<<filename2<<endl;
+    }
+    cout<<"File Descriptor: "<<fp2<<endl;
+	for(int i = 0 ; i < trainingData.rows ; i++)
+	{
+		for(int j = 0 ; j < trainingData.cols ; j++)
+		{
+			  fp2<<trainingData.ptr<float>(i)[j] << '\t';//寫入字串
+		}
+		fp2<<endl;
+	}
+  
+ 
+    fp2.close();//關閉檔案
+
+
+	//save pca & lda
+	FileStorage fo("pca.yaml", FileStorage::WRITE);
+				 if(fo.isOpened()) {
+					 fo << "pca mean" << pca.mean;
+					 fo << "pca eigenvalues" << pca.eigenvalues;
+					 fo << "pca eigenvectors" << pca.eigenvectors;
+				 }
+	fo.release();
+	lda.save("lda.yaml");
     // Set up SVM's parameters
     CvSVMParams params;
     params.svm_type    = CvSVM::C_SVC;
-	params.kernel_type = CvSVM::LINEAR;
-    //params.term_crit   = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
+    params.kernel_type = CvSVM::LINEAR;
+    params.term_crit   = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
+
+
 
     // Train the SVM
+
     CvSVM SVM;
-    if (SVM.train_auto(trainingData, labels, Mat(), Mat(), params)) {
+    if (SVM.train_auto(transform, labels, Mat(), Mat(), params)) {
 		SVM.save("svm_data_auto.xml");
 	}
+	
 }
 
 float MySVM::testSVM()
@@ -83,11 +156,48 @@ float MySVM::testSVM()
 	for (unsigned int i = 0; i < testVector.size(); ++i) {
 		testImage.at<float>(0, i) = testVector[i];
 	}
-
 	CvSVM SVM;
 	SVM.load(modelFile.c_str());
 
-	return SVM.predict(testImage, false); // test result 
+
+	//read pca & lda
+	string pca_file = "pca.yaml";
+	FileStorage fs(pca_file, FileStorage::READ);
+
+	PCA pca;
+
+	fs["pca mean"] >> pca.mean;   // Read entire cv::Mat
+	fs["pca eigenvalues"] >> pca.eigenvalues;   // Read entire cv::Mat
+	fs["pca eigenvectors"] >> pca.eigenvectors;   // Read entire cv::Mat
+
+	LDA lda;
+	lda.load("lda.yaml");
+
+	//test data
+	transform = lda.project(pca.project(testImage));
+
+	transform.convertTo(transform, CV_32FC1);
+
+	//write
+	char filename[]="test.txt";
+    fstream fp;
+    fp.open(filename, ios::out);//開啟檔案
+	for(int i = 0 ; i < transform.rows ; i++)
+	{
+		for(int j = 0 ; j < transform.cols ; j++)
+		{
+			fp<<transform.ptr<float>(i)[j] << '\t';//寫入字串
+		}
+		fp<<endl;
+	}
+  
+ 
+    fp.close();//關閉檔案
+
+
+
+	
+	return SVM.predict(transform, true); // test result 
 }
 
 void MySVM::setModel(string filename)
@@ -99,3 +209,4 @@ void MySVM::clear_testVector()
 {
 	testVector.clear();
 }
+
--- MySVM.h
+++ MySVM.h
@@ -4,6 +4,7 @@
 #include "opencv2\core\core.hpp"
 #include "opencv2\highgui\highgui.hpp"
 #include "opencv2\ml\ml.hpp"
+#include "opencv2/contrib/contrib.hpp"
 #include <iostream>
 #include <fstream>
 
@@ -22,10 +23,12 @@ public:
 	float testSVM();
 	void setModel(string filename);
 	void clearVectors();
+	Mat project(const Mat &,const Mat&,const Mat&);
 
 private:
 	vector< vector<float> > gtVectors;
 	vector< vector<float> > otherVectors;
 	vector<float> testVector;
 	string modelFile;
+	Mat transform;
 };
